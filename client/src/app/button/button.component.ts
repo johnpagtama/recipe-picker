a17913b9616ef33d3ccc9592c071400f7d76a0d6
johnpagtama <pagtamajr@gmail.com>
2024-03-10 01:48:04 -0800

    Added button component

---
+++ client/src/app/button/button.component.ts
@@ -0,0 +1,23 @@
+import { NgClass } from '@angular/common';
+import { Component, Input } from '@angular/core';
+
+@Component({
+  selector: 'app-button',
+  standalone: true,
+  imports: [NgClass],
+  templateUrl: './button.component.html',
+  styleUrl: './button.component.css',
+})
+export class ButtonComponent {
+  @Input() type: string = 'button';
+  @Input() text: string = 'Button';
+  @Input() name: string = 'button';
+  @Input() class: string = '';
+
+  onClick(event: MouseEvent) {
+    event.preventDefault();
+    console.log(
+      `MouseEvent: ${event.type}\nType: ${this.type}\nText: ${this.text}\nName: ${this.name}\nClass: ${this.class}`
+    );
+  }
+}
