eaae2b653721f7456819b4c7735e0437d1a16413
johnpagtama <pagtamajr@gmail.com>
2024-03-14 03:04:09 -0700

    Injected recipe service

--- client/src/app/app.component.ts
+++ client/src/app/app.component.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Subscription } from 'rxjs';
+import { RecipeService } from './recipe.service';
 import { ButtonComponent } from './button/button.component';
 import { InputComponent } from './input/input.component';
 
@@ -10,6 +12,26 @@ import { InputComponent } from './input/input.component';
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
 })
-export class AppComponent {
+export class AppComponent implements OnInit, OnDestroy {
   title = 'client';
+
+  private recipeSubscription?: Subscription;
+
+  constructor(private recipeService: RecipeService) {}
+
+  ngOnInit(): void {
+    this.getRecipe();
+  }
+
+  ngOnDestroy(): void {
+    this.recipeSubscription?.unsubscribe();
+  }
+
+  getRecipe(): void {
+    this.recipeSubscription = this.recipeService
+      .getRecipe()
+      .subscribe((response) => {
+        console.log(response);
+      });
+  }
 }
