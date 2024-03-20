2b9374aa880214a440c33a56a93ece252bb8b021
johnpagtama <pagtamajr@gmail.com>
2024-03-19 21:26:16 -0700

    Added Ingredient model

---
+++ Server/Server/Models/Ingredient.cs
@@ -0,0 +1,14 @@
+﻿namespace Server.Models
+{
+    public class Ingredient
+    {
+        public string? Text { get; set; }
+        public float Quantity { get; set; }
+        public string? Measure { get; set; }
+        public string? Food { get; set; }
+        public float Weight { get; set; }
+        public string? FoodCategory { get; set; }
+        public string? FoodId { get; set; }
+        public string? ImageUrl { get; set; }
+    }
+}
