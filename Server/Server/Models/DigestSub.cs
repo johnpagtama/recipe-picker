9c35f6c00bef4922a7fa72286070a1a4649bbd3f
johnpagtama <pagtamajr@gmail.com>
2024-03-19 21:22:05 -0700

    Added DigestSub model

---
+++ Server/Server/Models/DigestSub.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Models
+{
+    public class DigestSub
+    {
+        public string? Tag { get; set; }
+        public string? SchemaOrgTag { get; set; }
+        public float Total { get; set; }
+        public bool HasRDI { get; set; }
+        public float Daily { get; set; }
+    }
+}
