acaad234d8dcb3e5e4118a8a6745ea4af7dc7764
johnpagtama <pagtamajr@gmail.com>
2024-03-19 21:42:05 -0700

    Added RootObject model

---
+++ Server/Server/Models/RootObject.cs
@@ -0,0 +1,11 @@
+﻿namespace Server.Models
+{
+    public class RootObject
+    {
+        public int From { get; set; }
+        public int To { get; set; }
+        public int Count { get; set; }
+        public LinkNext? _Links { get; set; }
+        public Hit[]? Hits { get; set; }
+    }
+}
