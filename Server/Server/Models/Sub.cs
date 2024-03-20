39ffcb533b46c8ef94d0e43f869c5099e4098690
johnpagtama <pagtamajr@gmail.com>
2024-03-19 21:42:40 -0700

    Added Sub model

---
+++ Server/Server/Models/Sub.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Models
+{
+    public class Sub
+    {
+        public string? Label { get; set; }
+        public DigestSub? Info { get; set; }
+        public string? SubUnit { get; set; }
+    }
+}
