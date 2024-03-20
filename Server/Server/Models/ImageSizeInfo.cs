fe67b07fc89a60ee1fc2e4f9ef732e1d8138cb8a
johnpagtama <pagtamajr@gmail.com>
2024-03-19 21:23:44 -0700

    Added ImageSizeInfo model

---
+++ Server/Server/Models/ImageSizeInfo.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Models
+{
+    public class ImageSizeInfo
+    {
+        public string? ImageSizeInfoUrl { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+    }
+}
