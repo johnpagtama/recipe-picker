1c22f1a2f7256caa4818f2ba04c084a47474932c
johnpagtama <pagtamajr@gmail.com>
2024-03-19 21:41:44 -0700

    Added Nutrient model

---
+++ Server/Server/Models/Nutrient.cs
@@ -0,0 +1,9 @@
+﻿namespace Server.Models
+{
+    public class Nutrient
+    {
+        public string? Label { get; set; }
+        public float Quantity { get; set; }
+        public string? Unit { get; set; }
+    }
+}
