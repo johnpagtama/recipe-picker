4fde5ad55219c3094b3994489f35026af2b015c5
johnpagtama <pagtamajr@gmail.com>
2024-03-19 21:44:03 -0700

    Added TotalNutrients model

---
+++ Server/Server/Models/TotalNutrients.cs
@@ -0,0 +1,15 @@
+﻿namespace Server.Models
+{
+    public class TotalNutrients
+    {
+        public Nutrient? FATRN { get; set; }
+        public Nutrient? FAMS { get; set; }
+        public Nutrient? FAPU { get; set; }
+        public Nutrient? CHOCDFnet { get; set; }
+        public Nutrient? SUGAR { get; set; }
+        public Nutrient? FOLFD { get; set; }
+        public Nutrient? FOLAC { get; set; }
+        public Nutrient? WATER { get; set; }
+        public Nutrient? SUGARadded { get; set; }
+    }
+}
