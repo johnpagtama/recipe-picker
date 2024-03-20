3b6676e6b81194505633ff67acc2e88dfd08ede3
johnpagtama <pagtamajr@gmail.com>
2024-03-19 21:23:17 -0700

    Added ImageSize model

---
+++ Server/Server/Models/ImageSize.cs
@@ -0,0 +1,10 @@
+﻿namespace Server.Models
+{
+    public class ImageSize
+    {
+        public ImageSizeInfo? Thumbnail { get; set; }
+        public ImageSizeInfo? Small { get; set; }
+        public ImageSizeInfo? Regular { get; set; }
+        public ImageSizeInfo? Large { get; set; }
+    }
+}
