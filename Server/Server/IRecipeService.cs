a83c611fd6ba278d5ce6f5f74d3d4572a428b122
johnpagtama <pagtamajr@gmail.com>
2024-03-25 13:43:37 -0700

    Added RecipeService which implements IRecipeService

---
+++ Server/Server/IRecipeService.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server
+{
+    public interface IRecipeService
+    {
+        Task<ActionResult<string>> GetRecipes(string? type, string? appId, string? appKey, string? query);
+    }
+}
---
+++ Server/Server/RecipeService.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Server
+{
+    public class RecipeService : IRecipeService
+    {
+        private readonly HttpClient _httpClient;
+
+        public RecipeService(HttpClient httpClient)
+        {
+            _httpClient = httpClient;
+        }
+
+        public async Task<ActionResult<string>> GetRecipes(string? type, string? appId, string? appKey, string? query)
+        {
+            return await _httpClient.GetStringAsync($"?type={type}&q={query}&app_id={appId}&app_key={appKey}");
+        }
+    }
+}
