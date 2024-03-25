03b40790a1e908e50894f748b78ad786589fa03c
johnpagtama <pagtamajr@gmail.com>
2024-03-25 13:44:38 -0700

    Added RecipeController

---
+++ Server/Server/Controllers/RecipeController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Cors;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Server.Controllers
+{
+    [EnableCors("AllowedOrigins")]
+    [ApiController]
+    [Route("[controller]")]
+    public class RecipeController : Controller
+    {
+        private readonly IConfiguration _configuration;
+        private readonly IRecipeService _recipeService;
+
+        public RecipeController(IConfiguration configuration, IRecipeService recipeService)
+        {
+            _configuration = configuration;
+            _recipeService = recipeService;
+        }
+
+        // GET: RecipeController
+        [HttpGet(Name = "GetRecipes")]
+        public async Task<ActionResult<string>> GetRecipes()
+        {
+            return await _recipeService.GetRecipes("public", _configuration["Recipe:AppID"], _configuration["Recipe:AppKey"], "beef");
+        }
+    }
+}
